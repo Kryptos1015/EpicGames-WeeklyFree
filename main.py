b32637f4852e157e72f647bba6522930489b36db
Rahma A <64001826+Kryptos1015@users.noreply.github.com>
2021-10-19 14:41:55 +0400

    get_game_info()
    - gets all info from free games that will be needed later for the discord bot

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from requests import get
 from webbrowser import open_new
 
-free_games = []
+free_games = {}
+info_link = 'https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions'
 
-def get_games():
-    request = get('https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions').json()
+# finds the 2 weekly free games, collects game info for bot
+def get_games_info():
+    request = get(info_link).json()
     request = request['data']['Catalog']['searchStore']['elements']
 
     for game in request:
-        if game['price']['totalPrice']['discount'] != 0:
-            free_games.append('https://www.epicgames.com/store/p/' + game['urlSlug'])
+        original_price = game['price']['totalPrice']['originalPrice']
+        discount = game['price']['totalPrice']['discount']
 
-print(free_games)
+        # len of lists change, loop needs to be used to find game image
+        if discount != 0 and discount == original_price:
+            for image in game['keyImages']:
+                if 'OfferImageWide' in image.values():
+                    embed_image = image['url']
+
+            # organizing all info neatly into dicts :DDD
+            free_games[game['title']] = {'link': 'https://www.epicgames.com/store/p/' + game['urlSlug'],
+                                        'description': game['description'],
+                                        'start_date': game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['startDate'],
+                                        'end_date': game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['endDate'],
+                                        'image': embed_image}
+
+
+get_games_info()
